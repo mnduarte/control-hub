c023ada655e34dcb53b2301d7e676bbfa8e65596
Matias Duarte <33585271+mnduarte@users.noreply.github.com>
2021-05-28 12:34:11 -0300

    Quit innecesary return

--- src/controllers/fibonacci.controller.js
+++ src/controllers/fibonacci.controller.js
@@ -1,9 +1,8 @@
-const buildFibonacci = (_idx) => {
-  return new Array(_idx + 1).fill(1).reduce((arr, val, i) => {
+const buildFibonacci = (_idx) =>
+  new Array(_idx + 1).fill(1).reduce((arr, val, i) => {
     arr.push(i <= 1 ? i : arr[i - 2] + arr[i - 1]);
     return arr;
   }, []);
-};
 
 const errorMapping = {
   MISSING_PARAMS: {
